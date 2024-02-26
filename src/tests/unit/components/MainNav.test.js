d5db347a8e6d5a3e98e79dfe175f5ac43fc971d2
Peps <pecarcer@gmail.com>
2024-02-26 12:54:49 +0100

    Added NavBar and test

--- src/tests/unit/components/MainNav.test.js
+++ src/tests/unit/components/MainNav.test.js
@@ -8,4 +8,18 @@ describe('MainNav', () => {
     const companyName = screen.getByText('Mochi Careers')
     expect(companyName).toBeInTheDocument()
   })
+
+  it('displays menu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuitems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuitems.map((item) => item.textContent)
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Mochi Careers',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 })
